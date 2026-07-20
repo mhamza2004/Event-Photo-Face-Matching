97df2aa92f714cf409a9c87a93dd0f9d507365e8
Muhammad Hamza <m.hamza33004@gmail.com>
2026-07-20 13:03:01 +0500

    Fix duplicate api exports

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,40 +1,17 @@
 import axios from "axios";
 
-import axios from "axios";
-
 const api = axios.create({
-    baseURL:
-        import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
 });
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
+  return config;
 });
-
-export default api;
-
-api.interceptors.request.use(
-
-(config)=>{
-
-const token=localStorage.getItem("token");
-
-if(token){
-
-config.headers.Authorization=`Bearer ${token}`;
-
-}
-
-return config;
-
-}
-
-);
 
 export default api;
